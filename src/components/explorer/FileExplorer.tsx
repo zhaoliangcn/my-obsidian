2e3141177affc96ed6a6244d1da9c8542112f59e
zhaoliang <zhaoliangcn@126.com>
2026-05-23 10:01:05 +0800

    修复编译错误

--- src/components/explorer/FileExplorer.tsx
+++ src/components/explorer/FileExplorer.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useRef, useEffect, useCallback } from 'react'
+import { useState, useMemo, useRef, useEffect } from 'react'
 import { useStore } from '../../store'
 import type { TreeNode, Folder, NoteRef } from '../../types'
 import {
